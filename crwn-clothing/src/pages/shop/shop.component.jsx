b19f3c075a9421403eb2fe30386018f4f13f7b78
Xiaoting Li <xiaotingli@Xiaotings-MacBook-Air.local>
2020-07-30 17:24:31 -0400

    move our directory and shop data into its own reducer

--- crwn-clothing/src/pages/shop/shop.component.jsx
+++ crwn-clothing/src/pages/shop/shop.component.jsx
@@ -1,28 +1,15 @@
+
 import React from 'react';
-import SHOP_DATA from './shop.data';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import { Route } from 'react-router-dom';
 
-class Shop extends React.Component {
-    constructor(props) {
-        super(props)
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
 
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
+const ShopPage = ({ match }) => (
+  <div className='shop-page'>
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+  </div>
+);
 
-    render() {
-        const { collections } = this.state;
-        return (
-            <div className='shop-page'>
-                {
-                    collections.map(({id, ...oteherCollectionProps}) => (
-                        <CollectionPreview key={id} {...oteherCollectionProps}/>
-                    ))
-                }
-            </div>
-        );
-    }
-}
-
-export default Shop;
+export default ShopPage;
