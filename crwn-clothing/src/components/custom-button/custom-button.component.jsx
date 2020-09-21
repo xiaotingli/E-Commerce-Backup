655265bddecae1bd0386a75263ceded0fcda3160
Xiaoting Li <xiaotingli@Xiaotings-MacBook-Air.local>
2020-09-20 21:15:35 -0400

    update

--- crwn-clothing/src/components/custom-button/custom-button.component.jsx
+++ crwn-clothing/src/components/custom-button/custom-button.component.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
-import './custom-button.styles.scss';
+import { CustomButtonContainer } from './custom-button.styles';
 
-const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherProps}) => {
-    return (
-        <button className={`${inverted ? 'inverted' : ''} ${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...otherProps}>
-            { children }
-        </button>
-    );
-};
+const CustomButton = ({ children, ...otherProps}) => (
+    <CustomButtonContainer {...otherProps}>{ children }</CustomButtonContainer>
+);
 
 export default CustomButton;
